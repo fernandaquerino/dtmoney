8304999a3a168e2882de379db8c3438208ad69b4
Fernanda Querino <fequerinodelima@gmail.com>
2022-01-08 03:19:50 -0300

    add: calculando os totais do summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -12,6 +12,22 @@ export function Summary() {
 
   console.log(transactions);
 
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.widthdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+
+    return acc;
+  }, {
+    deposits: 0,
+    widthdraws: 0,
+    total: 0,
+  })
+
   return (
     <Container>
       <div>
@@ -19,21 +35,37 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR',  {
+                  style: 'currency',
+                  currency: 'BRL'
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saidas" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          - 
+          {new Intl.NumberFormat('pt-BR',  {
+                  style: 'currency',
+                  currency: 'BRL'
+          }).format(summary.widthdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR',  {
+                  style: 'currency',
+                  currency: 'BRL'
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   )
